4b8f90543e75e4d6928e7940dbb56bae1ca80958
Cristian Martinez Toledo <cristian@imac.local>
2015-09-30 16:50:52 -0300

    Initial Commit

---
+++ Centro de Liderazgo Ejercito de ChileTests/Centro_de_Liderazgo_Ejercito_de_ChileTests.swift
@@ -0,0 +1,36 @@
+//
+//  Centro_de_Liderazgo_Ejercito_de_ChileTests.swift
+//  Centro de Liderazgo Ejercito de ChileTests
+//
+//  Created by Cristian Martinez Toledo on 30-09-15.
+//  Copyright © 2015 Cristian Martinez Toledo. All rights reserved.
+//
+
+import XCTest
+@testable import Centro_de_Liderazgo_Ejercito_de_Chile
+
+class Centro_de_Liderazgo_Ejercito_de_ChileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
