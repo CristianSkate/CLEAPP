32994c580a9b793ae5fbe6644c3e212f8d9414fc
Cristian Martinez Toledo <cristian@imac.local>
2015-10-01 19:35:38 -0300

    Version Inicial, login

---
+++ Centro de Liderazgo Ejercito de Chile/InicioViewController.swift
@@ -0,0 +1,59 @@
+//
+//  InicioViewController.swift
+//  Centro de Liderazgo Ejercito de Chile
+//
+//  Created by Cristian Martinez Toledo on 30-09-15.
+//  Copyright © 2015 Cristian Martinez Toledo. All rights reserved.
+//
+
+import UIKit
+
+class InicioViewController: UIViewController {
+
+    @IBOutlet weak var txtMensaje: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Inicio"
+        self.navigationController?.navigationBar.barTintColor =  UIColor(red: 215.0/255.0, green: 23.0/255.0, blue: 41.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func viewDidAppear(animated: Bool) {
+        
+        // creamos un objeto de tipo NSUserDefaults prefs (caché) que guardará si el usuario está logueado o no
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        // generamos una constante de tipo int leyendo de NSUserDefaults ISLOGGEDIN
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        
+        // si no está logeado, envía a la vista de login, sino muestra el nombre de usuario, leido de la caché
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("irALogin", sender: self)
+        } else {
+            let mensaje = "Bienvenido " + (prefs.valueForKey("NOMBRE") as? String)! + " " + (prefs.valueForKey("APELLIDOPATERNO") as? String)! + " " + (prefs.valueForKey("APELLIDOMATERNO") as? String)! + " estamos trabajando para usted"
+            self.txtMensaje.text =  mensaje
+        }
+        
+    }
+    // La función cerrar borrar todos los datos de la caché y devuelve al login
+    
+    @IBAction func cerrar(sender: UIButton) {
+        
+        
+        let appDomain = NSBundle.mainBundle().bundleIdentifier
+        
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+        
+        self.performSegueWithIdentifier("irALogin", sender: self)
+        
+    }
+
+}
---
+++ Centro de Liderazgo Ejercito de Chile/LoginViewController.swift
@@ -0,0 +1,207 @@
+//
+//  ViewController.swift
+//  Centro de Liderazgo Ejercito de Chile
+//
+//  Created by Cristian Martinez Toledo on 30-09-15.
+//  Copyright © 2015 Cristian Martinez Toledo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, NSXMLParserDelegate {
+
+    
+    @IBOutlet weak var txtUsuario: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    var urlData:NSData!
+    var parser = NSXMLParser()
+    var posts = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var passData =  false
+    var nombre:NSString = ""
+    var apellidoPaterno:NSString = ""
+    var apellidoMaterno:NSString = ""
+    var resp:NSString = ""
+    var responseError:NSError?
+    var intento = 1
+    var response: NSURLResponse?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func btnIniciarSesion(sender: AnyObject) {
+        iniciarSesion()
+    }
+    
+    
+    func iniciarSesion() {
+        
+        let user:NSString = txtUsuario.text!
+        let pass: NSString = txtPassword.text!
+        
+        if(user == "") || (pass == "") {
+            
+            let alertController = UIAlertController(title: "¡Ups!", message: "Debe ingresar run sin puntos y contraseña", preferredStyle: .Alert)
+            alertController.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+        }else{
+            
+            let url:NSURL = NSURL(string: "http://cle.ejercito.cl/ServiciosCle.asmx/Login")! // llamamos a la URl donde está el ws que se conectará con la BD
+            let request:NSMutableURLRequest = NSMutableURLRequest(URL: url) // componemos la URL con una var request y un NSMutableURLRequest y le pasamos como parámetros las vars
+            request.HTTPMethod = "POST"
+            let post:NSString = "run=\(user)&pass=\(pass)" // se mete el user y pass dentro de un string
+            NSLog("PostData: %@",post) // mandamos al log para ir registrando lo que va pasando
+            let postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)! // codificamos lo que se envía
+            let postLength:NSString = String( postData.length ) // se determina el largo del string
+            request.HTTPBody = postData
+            request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            request.setValue("cle.ejercito.cl", forHTTPHeaderField: "Host")
+            
+            // hacemos la conexion
+            do {
+                urlData = try NSURLConnection.sendSynchronousRequest(request, returningResponse:&response)
+            } catch let error as NSError {
+                self.responseError = error
+                urlData = nil
+            }
+            
+            // se valida
+            if ( urlData != nil ) {
+                let res = response as! NSHTTPURLResponse!;
+          
+                NSLog("Response code: %ld", res.statusCode)
+                if res.statusCode == 500 && intento == 1 {
+                    intento += 1
+                    iniciarSesion()
+                    return
+                    
+                }
+                
+                if (res.statusCode >= 200 && res.statusCode < 300)
+                {
+                    let responseData:NSString  = NSString(data:urlData!, encoding: NSUTF8StringEncoding)!
+                   
+                    NSLog("Response ==> %@", responseData);
+                    
+                    
+                    // obtenemos de json el valor success y lo tratamos cono int
+                    var success:NSInteger = 0
+                    
+                    parsearXML()
+                    if (resp.isEqualToString("Error")){
+                       success = 0
+                    }else{
+                       success = 1
+                    }
+                    
+                    if(success == 1)
+                    {
+                        NSLog("Login Correcto");
+                        // guardamos en la caché
+                        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+                        // tomamos el nombre de usuario y lo guardamos
+                        prefs.setObject(user, forKey: "RUN")
+                        // también si está logeado
+                        prefs.setInteger(1, forKey: "ISLOGGEDIN")
+                        // se sincroniza
+                        prefs.setValue(apellidoMaterno, forKey: "APELLIDOMATERNO")
+                        prefs.setValue(apellidoPaterno, forKey: "APELLIDOPATERNO")
+                        prefs.setValue(nombre, forKey: "NOMBRE")
+                        prefs.synchronize()
+                        
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    } else {
+                        let error_msg:NSString = "Run o Password incorrectos"
+                        
+                        let alertController = UIAlertController(title: "¡Ups!", message: error_msg as String, preferredStyle: .Alert)
+                        alertController.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
+                        self.presentViewController(alertController, animated: true, completion: nil)
+                    }
+                    
+                } else {
+                    let alertController = UIAlertController(title: "¡Ups!", message: "Conexión fallida", preferredStyle: .Alert)
+                    alertController.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                }
+            } else {
+                let alertController = UIAlertController(title: "¡Ups!", message: "Conexión fallida", preferredStyle: .Alert)
+                alertController.addAction(UIAlertAction(title: "Aceptar", style: .Default, handler: nil))
+                
+                if let error = responseError {
+                    alertController.message = (error.localizedDescription)
+                }
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    // funcion que la que limpiamos los campos de texto
+    
+    @IBAction func limpiar(sender: UIButton) {
+        
+        let user:NSString = txtUsuario.text!
+        let pass: NSString = txtPassword.text!
+        
+        if((user != "") || (pass != "")){
+            
+            txtUsuario.text = ""
+            txtPassword.text = ""
+        }
+    }
+    
+    //funciones para parsear la respuesta XML
+    func parsearXML() {
+        posts = []
+        parser =  NSXMLParser(data: urlData!)
+        parser.delegate =  self
+        parser.parse()
+    }
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        element =  elementName
+        if(elementName=="paterno" || elementName=="materno" || elementName=="nombres" || elementName == "resp")
+        {
+            passData=true;
+        }
+    }
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        element="";
+        if(elementName=="paterno" || elementName=="materno" || elementName=="nombres" || elementName == "resp")
+        {
+            passData=false;
+        }
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        if (passData) {
+            
+            if element.isEqualToString("paterno"){
+                apellidoPaterno = string
+                NSLog("Apellido paterno ==> %@", string)
+            }else if element.isEqualToString("materno") {
+                apellidoMaterno = string
+                NSLog("Apellido materno ==> %@", string)
+            }else if element.isEqualToString("nombres"){
+                nombre = string
+                NSLog("Nombres ==> %@", string)
+            }else if element.isEqualToString("resp"){
+                resp = string
+                NSLog("Error ==> %@", string)
+            }
+        }
+    }
+}
+
